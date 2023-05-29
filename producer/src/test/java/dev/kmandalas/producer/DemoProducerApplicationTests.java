dfea331718f4c114c1e497231b599f0d10698696
Kyriakos Mandalas <kmandalas@cognity.gr>
2023-05-29 14:12:48 +0300

    added TestContainers

--- producer/src/test/java/dev/kmandalas/producer/DemoProducerApplicationTests.java
+++ producer/src/test/java/dev/kmandalas/producer/DemoProducerApplicationTests.java
@@ -2,9 +2,18 @@ package dev.kmandalas.producer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class DemoProducerApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:14.1-alpine");
 
 	@Test
 	void contextLoads() {
